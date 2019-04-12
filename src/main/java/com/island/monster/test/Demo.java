b9647214a30b59e6d37cb17d4afd28f8f9d27130
yaoguopeng <helloyaogp@163.com>
2019-04-12 16:23:16 +0800

    add ignore

--- src/main/java/com/island/monster/test/Demo.java
+++ src/main/java/com/island/monster/test/Demo.java
@@ -4,7 +4,7 @@ import com.island.monster.common.IslandUtil;
 
 public class Demo {
 
-    public static void main(String args []){
-        System.out.println("currentDate:"+IslandUtil.currentDate());
-    }
+//    public static void main(String args []){
+//        System.out.println("currentDate:"+IslandUtil.currentDate());
+//    }
 }
