b1c3121e518d2722c2c195f592117e2403e4a320
yaoguopeng <helloyaogp@163.com>
2019-05-07 13:31:01 +0800

    热部署依赖添加版本信息

--- src/test/java/com/island/monster/test/Demo.java
+++ src/test/java/com/island/monster/test/Demo.java
@@ -9,12 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.lang.reflect.InvocationHandler;
 import java.util.ArrayList;
 import java.util.List;
 
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
 public class Demo {
+
 
     class Cat{
         private String name;
